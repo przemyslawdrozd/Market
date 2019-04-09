102da011fe7431496224835ff57d7c807e7e078a
przemo06133 <przemo06133@gmail.com>
2019-04-10 01:21:43 +0200

    IO Test Write and Read List of Items

--- src/com/company/TestIO.java
+++ src/com/company/TestIO.java
@@ -1,13 +1,85 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class TestIO {
 
     public static void main(String[] args) {
 
-        int i = 5;
+        List<Item> itemsWrite = createList();
+        try (FileWriter fileWriter = new FileWriter("Items.txt")){
+            for (Item i: itemsWrite) {
+                fileWriter.write(i.getNameOfProduct() + "," + i.getPrice() + "," + i.getQuantity() + "\n");
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("i = " + i);
+
+        List<Item> itemsRead = new ArrayList<>();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new FileReader("Items.txt"));
+            scanner.useDelimiter(",");
+
+            while (scanner.hasNextLine()) {
+
+                String name = scanner.next();
+                scanner.skip(scanner.delimiter());
+
+                double price = Double.valueOf(scanner.next());
+                scanner.skip(scanner.delimiter());
+
+                int quantity = Integer.valueOf(scanner.nextLine());
+                itemsRead.add(new Item(name, price, quantity));
+            }
+
+            for (Item i: itemsRead) {
+                System.out.println(i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
 
 
+    private static List<Item> createList() {
+        List<Item> items = new ArrayList<>();
+
+        items.add(new Item("Banana", 12.32, 40));
+        items.add(new Item("Apple", 1.24, 30));
+        items.add(new Item("Beer", 5.37, 12));
+
+        return items;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
